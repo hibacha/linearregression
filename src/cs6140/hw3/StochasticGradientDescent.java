b549506943a829336d37a7af9c0bede17f3b1924
bachaPro <zhouyaofei@gmail.com>
2013-10-10 17:33:58 -0400

    Rename

---
+++ src/cs6140/hw3/StochasticGradientDescent.java
@@ -0,0 +1,47 @@
+package cs6140.hw3;
+
+import java.util.Arrays;
+
+
+
+public class StochasticGradientDescent extends BaseGradientDescent{
+
+	/**
+	 * @param args
+	 */
+
+	
+
+	public void trainData(double alpha, double convergeTolerance) {
+		boolean isConverge = false;
+		double oldRMSE = rmse();
+		System.out.println("rmse:"+oldRMSE);
+		double newRMSE = 0;
+		while (!isConverge) {
+			for (Email mail : normalizedSet) {
+				//update weight with newly calculated weight
+				weight = Arrays.copyOf(newWeight, 57);
+				for (int j = 0; j < 57; j++) {
+					double diff = mail.get(57) - h(weight, mail);
+					newWeight[j] = weight[j] + alpha * diff * mail.get(j);
+				}
+			}
+			newRMSE = rmse();
+			isConverge= isConverge(oldRMSE,newRMSE,convergeTolerance);
+			oldRMSE=newRMSE;
+		}
+		System.out.println("Final RMSE"+oldRMSE);
+	}
+
+
+	
+	
+	public static void main(String[] args) {
+		double lambda =0.001;
+		double convergeTolerance=0.00001;
+		boolean isPrintRMSE = true;
+		boolean isPrintWeight = true;
+		runner(new StochasticGradientDescent(), lambda, convergeTolerance, isPrintWeight, isPrintRMSE);
+	}
+
+}
