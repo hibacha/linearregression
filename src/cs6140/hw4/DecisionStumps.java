1817b6b5ac35a43198bc85a79b519fbc65861429
bachaPro <zhouyaofei@gmail.com>
2013-10-21 19:51:26 -0400

    get optimum for first round iteration

--- src/cs6140/hw4/DecisionStumps.java
+++ src/cs6140/hw4/DecisionStumps.java
@@ -1,15 +1,34 @@
 package cs6140.hw4;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Vector;
 
 import cs6140.hw3.Email;
 
 public class DecisionStumps {
-	public Vector<Email> trainingSet;
+	public ArrayList<Email> trainingSet;
 	public int optimumFeature;
-	public Vector<Double> d;
+	
+	//distribution
+	public Vector<Double> d= new Vector<Double>();
+	
+	//this array store the sorted data by each feature value
+	public ArrayList<Vector<Integer>> sortedByFeatureData = new ArrayList<Vector<Integer>>();
+	
+	//this array store the threshold value for each feature value
+	public ArrayList<Vector<Double>> thresholdForFeature = new ArrayList<Vector<Double>>();
+	
+	public ArrayList<Email> getTrainingSet() {
+		return trainingSet;
+	}
+
+	public DecisionStumps(ArrayList<Email> trainingSet) {
+		this.trainingSet= trainingSet;
+		initD();
+	}
 	
 	public int train(Vector<Double> distribute){
 		return 0;
@@ -19,8 +38,86 @@ public class DecisionStumps {
 		
 	}
 	
-	public void sortByFeature(int fIndex){
-	    Collections.sort(null,new AdaComparator(fIndex));
-	    
+	public void initSortedFeatureArray(){
+		for (int i = 0; i < 57; i++) {
+			sortByFeature(i);
+		}
 	}
+	
+	public void initD(){
+		int m=trainingSet.size();
+		for (int i = 0; i < m; i++) {
+			d.add(1.0/m);
+		}
+	}
+	
+	
+	public void buildErrorMatrixForOneFeature(int featureIndex, Vector<Double> threshold, Vector<Integer> sortedDataPoint){
+		backToOriginalTrainingSet();
+		Iterator<Double> it = threshold.iterator();
+	   	List<Double> container = new ArrayList<Double>();
+	   	while(it.hasNext()){
+	   		Double athreshold = it.next();
+	   		
+	   		container.add(calculateErrorRateByWeight(featureIndex,
+					athreshold, sortedDataPoint));		
+	   		
+	   	}
+	   	System.out.println(Collections.min(container));
+	}
+	
+	private double calculateErrorRateByWeight(int featureIndex, double threshold, Vector<Integer> sortedDataPoint){
+	    Iterator<Integer> it = sortedDataPoint.iterator();
+	   	double errorRateWeight=0;
+	   	while(it.hasNext()){
+	   		Integer dataSeqId = it.next();
+	   		if(trainingSet.get(dataSeqId).get(featureIndex)<threshold){
+	   			errorRateWeight+=checkError(dataSeqId,-1.0);
+	   		} else{
+	   			errorRateWeight+=checkError(dataSeqId, 1.0);
+	   		}
+	   	}
+	   	return errorRateWeight;
+	}
+	
+	private double checkError(Integer dataSeqId, double predict) {
+		if(trainingSet.get(dataSeqId).get(MyConstant.INDEX_EMAIL_SPAM_LABEL)!=predict){
+			return d.get(dataSeqId);
+		}else{
+			return 0;
+		}
+		
+	}
+
+	public void backToOriginalTrainingSet(){
+		sortByFeature(MyConstant.INDEX_FOR_DATA_ID);
+	}
+	
+	private  void sortByFeature(int fIndex){
+		
+	    Collections.sort(trainingSet,new AdaComparator(fIndex));
+	    
+	    Vector<Integer> sortedDataPointSeq = new Vector<Integer>();
+	    Vector<Double> threshold = new Vector<Double>();
+	    
+	    double previousFeatureValue=trainingSet.get(0).get(fIndex);
+	    threshold.add(Double.NEGATIVE_INFINITY);
+	    for(Email e:trainingSet){
+	    	sortedDataPointSeq.add((int)e.get(MyConstant.INDEX_FOR_DATA_ID).doubleValue());
+	    	double currentFeatureValue=e.get(fIndex);
+	    	
+	    	if(currentFeatureValue>previousFeatureValue){
+	    		double midPoint =(previousFeatureValue+currentFeatureValue)/2;
+	    		threshold.add(midPoint);
+	    		previousFeatureValue=currentFeatureValue;
+	    		//System.out.println("midPoint"+ midPoint);
+	    	}
+	    	//System.out.println(e.get(MyConstant.INDEX_FOR_DATA_ID) + "->"+e.get(0));
+	    }
+	    threshold.add(Double.POSITIVE_INFINITY);
+	    
+	    sortedByFeatureData.add(sortedDataPointSeq);
+	    thresholdForFeature.add(threshold);
+	}
+	
 }
--- src/cs6140/hw4/Runner.java
+++ src/cs6140/hw4/Runner.java
@@ -13,8 +13,21 @@ public class Runner {
 		ArrayList<Email> trainingSet = k.getTrainingData();
 		normalize(trainingSet);
 
+		DecisionStumps ds=new DecisionStumps(trainingSet);
+		ds.initSortedFeatureArray();
+		
+		for(int i=0;i<57;i++){
+			System.out.println("\n"+i);
+			ds.buildErrorMatrixForOneFeature(i,ds.thresholdForFeature.get(i),ds.sortedByFeatureData.get(i));
+		}
 	}
-
+	
+    /**
+     * add label Seq Id to each data point
+     * add transform spam label 0 to -1
+     * 
+     * @param trainingSet
+     */
 	public static void normalize(ArrayList<Email> trainingSet) {
 		for (int i = 0; i < trainingSet.size(); i++) {
 			if (trainingSet.get(i).get(MyConstant.INDEX_EMAIL_SPAM_LABEL) == 0) {
