5e7f1f44d6991830facb70b58786565f62223fd9
bachaPro <zhouyaofei@gmail.com>
2013-10-23 19:46:13 -0400

    copy email from hw3

---
+++ src/cs6140/hw4/Email.java
@@ -0,0 +1,12 @@
+package cs6140.hw4;
+
+import java.util.Vector;
+
+public class  Email extends Vector<Double> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7884547080684921937L;
+
+}
