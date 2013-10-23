f11924015d27e454ed9e2b64b7d76ae3907ecbf1
bachaPro <zhouyaofei@gmail.com>
2013-10-23 19:38:50 -0400

    add comment

--- src/cs6140/hw4/DecisionStumps.java
+++ src/cs6140/hw4/DecisionStumps.java
@@ -2,7 +2,6 @@ package cs6140.hw4;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Vector;
@@ -10,46 +9,30 @@ import java.util.Vector;
 import cs6140.hw3.Email;
 
 public class DecisionStumps {
+	
+	//store training dataset 
 	public ArrayList<Email> trainingSet;
-	public int optimumFeature;
 	
-	//distribution
-	private Vector<Double> d= new Vector<Double>();
+	//distribution for weighted error rate
+	private Vector<Double> distribution= new Vector<Double>();
+    
+	//a list indexed feature containing a list of ThresholdToErrorItem Object
+	private ArrayList<Vector<ThresholdToErrorItem>> allFeatureToThresholdsDic = new ArrayList<Vector<ThresholdToErrorItem>>();
 	
-	public Vector<Double> getD() {
-		return d;
-	}
-
-	public void setD(Vector<Double> d) {
-		this.d = d;
-	}
-
-	public ArrayList<Vector<ThresholdToErrorItems>> thresholdForFeature = new ArrayList<Vector<ThresholdToErrorItems>>();
-	
-	public ArrayList<Email> getTrainingSet() {
-		return trainingSet;
-	}
-
-	public DecisionStumps(ArrayList<Email> trainingSet) {
-		this.trainingSet= trainingSet;
-		initD();
-	}
-	
-	public int train(Vector<Double> distribute){
-		return 0;
-	}
-	
-	public void initD(){
+	//initialize as uniform distribution
+	public void initDistribution(){
 		int m=trainingSet.size();
 		for (int i = 0; i < m; i++) {
-			d.add(1.0/m);
+			distribution.add(1.0/m);
 		}
 	}
 	
+	//sort training set using data Id to make it consistent with distribution
 	public void backToOriginalTrainingSet(){
 		Collections.sort(trainingSet,new EmailFeatureComparator(MyConstant.INDEX_FOR_DATA_ID));
 	}
 	
+	//get global optimal solution among all feature's thresholds
 	public Solution getGlobalOptimalSolution(){
         ArrayList<Solution> container = new ArrayList<Solution>();
 		for(int i=0;i<57;i++){
@@ -58,11 +41,12 @@ public class DecisionStumps {
 		return extractOptimalSolution(container);
 	}
 	
+	//get optimal solution by given feature
 	public Solution getOptimalSolutionOfOneFeature(int featureIndex){
-		Vector<ThresholdToErrorItems> thresholds = thresholdForFeature.get(featureIndex);
+		Vector<ThresholdToErrorItem> thresholds = allFeatureToThresholdsDic.get(featureIndex);
         ArrayList<Solution> container = new ArrayList<Solution>();
-		for(ThresholdToErrorItems item: thresholds){
-			double errorRateWeighted=item.errorRate(d);
+		for(ThresholdToErrorItem item: thresholds){
+			double errorRateWeighted=item.errorRate(distribution);
 			double threshold=item.getThreshold();
 			Solution so=new Solution(featureIndex, errorRateWeighted, threshold, (double)item.getErrorIds().size()/trainingSet.size());
 			container.add(so);
@@ -70,27 +54,27 @@ public class DecisionStumps {
 		return extractOptimalSolution(container);
 	}
 	
+	//extract optimal from either max weighted error rate or min weighted error rate
 	private Solution extractOptimalSolution(List<Solution> container) {
 		Solution min= Collections.min(container, new SolutionComparator());
 	   	Solution max= Collections.max(container, new SolutionComparator());
-	   	if(Math.abs(0.5-min.getErrorRateWeighted())>Math.abs(0.5- max.getErrorRateWeighted()))
-	   	  return  min;
-	   	else
-	   	  return max;
+	   	return Math.abs(0.5-min.getErrorRateWeighted())>Math.abs(0.5- max.getErrorRateWeighted())? min:max;
 	}
 	
-	public  void generateEntireDictionary(){
+	//generate entire feature -> threshold dictionary
+	public void generateEntireDictionary(){
 		backToOriginalTrainingSet();
 		for (int i = 0; i < 57; i++) {
-			Vector<ThresholdToErrorItems> allThresholds  =this.thresholdForFeature.get(i);
+			Vector<ThresholdToErrorItem> allThresholds  =this.allFeatureToThresholdsDic.get(i);
 			for (int j = 0; j < allThresholds.size(); j++) {
-				ThresholdToErrorItems thresholdItem = allThresholds.get(j);
-				getWrongList(thresholdItem);
+				ThresholdToErrorItem thresholdItem = allThresholds.get(j);
+				setWrongList(thresholdItem);
 			}
 		}
 	}
 	
-	private void getWrongList(ThresholdToErrorItems thresholdObj){
+	//set wrong predicted data Id into ThresholdToErrorItem
+	private void setWrongList(ThresholdToErrorItem thresholdObj){
 		Iterator<Email> it=trainingSet.iterator();
 		int featureIndex = thresholdObj.getFeatureIndex();
 		double threshold = thresholdObj.getThreshold();
@@ -104,28 +88,48 @@ public class DecisionStumps {
 		}
 	}
 	
+	//calculate all thresholds
 	public void initSortedFeatureArray(){
 		for (int i = 0; i < 57; i++) {
 			sortByFeature(i);
 		}
 	}
 	
-	private  void sortByFeature(int fIndex){
+	//set mid point as threshold for given feature
+	private void sortByFeature(int fIndex){
 	    Collections.sort(trainingSet,new EmailFeatureComparator(fIndex));
-	    Vector<ThresholdToErrorItems> oneFeatureThresholdVec = new Vector<ThresholdToErrorItems>();
+	    Vector<ThresholdToErrorItem> oneFeatureThresholdVec = new Vector<ThresholdToErrorItem>();
 	    double previousFeatureValue=trainingSet.get(0).get(fIndex);
-	    oneFeatureThresholdVec.add(new ThresholdToErrorItems(Double.NEGATIVE_INFINITY, fIndex));
+	    oneFeatureThresholdVec.add(new ThresholdToErrorItem(Double.NEGATIVE_INFINITY, fIndex));
 	    for(Email e:trainingSet){
 	    	double currentFeatureValue=e.get(fIndex);
 	    	
 	    	if(currentFeatureValue>previousFeatureValue){
 	    		double midPoint =(previousFeatureValue+currentFeatureValue)/2;
-	    		oneFeatureThresholdVec.add(new ThresholdToErrorItems(midPoint, fIndex));
+	    		oneFeatureThresholdVec.add(new ThresholdToErrorItem(midPoint, fIndex));
 	    		previousFeatureValue=currentFeatureValue;
 	    	}
 	    }
-	    oneFeatureThresholdVec.add(new ThresholdToErrorItems(Double.POSITIVE_INFINITY, fIndex));
-	    thresholdForFeature.add(oneFeatureThresholdVec);
+	    oneFeatureThresholdVec.add(new ThresholdToErrorItem(Double.POSITIVE_INFINITY, fIndex));
+	    allFeatureToThresholdsDic.add(oneFeatureThresholdVec);
+	}
+	
+	public Vector<Double> getD() {
+		return distribution;
+	}
+
+	public void setD(Vector<Double> d) {
+		this.distribution = d;
+	}
+
+	
+	public ArrayList<Email> getTrainingSet() {
+		return trainingSet;
+	}
+
+	public DecisionStumps(ArrayList<Email> trainingSet) {
+		this.trainingSet= trainingSet;
+		initDistribution();
 	}
 	
 }
